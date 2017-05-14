e762011d1294d7e8aa6f9a07c940f25a50cf4aae
Chris Moulder <cmoulder@chicagobooth.edu>
2017-05-14 18:46:40 -0500

    final

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: { scope: :year,
       message: "Cannot have the same Movie title in the same year" }
   validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1870, less_than_or_equal_to: 2050 }
+  validates :duration, numericality: { only_integer: true, greater_than_or_equal_to: 0 , less_than_or_equal_to: 2764800, allow_blank: true }
   belongs_to :director
   has_many :characters
   has_many :actors, :through => :characters
