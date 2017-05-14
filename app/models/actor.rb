1b67e3df1653ee88ae7249f5c9cbad61ecf1c125
Chris Moulder <cmoulder@chicagobooth.edu>
2017-05-14 18:20:00 -0500

    v3

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,5 @@ class Actor < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :dob,
       message: "Cannot have more than one Actor with the same name and DOB" }
   has_many :characters
+  has_many :movies, :through => :characters
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,5 @@
 class Character < ApplicationRecord
   validates :movie_id, :actor_id, presence: true
+  belongs_to :movie
+  belongs_to :actor
 end
